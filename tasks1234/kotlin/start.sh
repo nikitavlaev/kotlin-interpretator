ad9efc1d53d981a4c9db634f7dfabc075f1c7a84
Nikita <nikitavlaev00@gmail.com>
2020-01-25 20:06:37 +0300

    Added bash support

--- tasks1234/kotlin/start.sh
+++ tasks1234/kotlin/start.sh
@@ -1,1 +1,13 @@
-stack run
+#!/bin/bash
+read -p "Write 'i' to start interpretator, 't' to start translator: " Input
+Name="test_jasmin"
+InFolder="test"
+OutFolder="jsmClasses"
+stack run -- $Input "$InFolder/$Name"
+if [ "$Input" == "t" ]; then 
+if [ -f "$OutFolder\$Name.class" ] #if exists
+then rm "$OutFolder\$Name.class"
+fi
+java -jar jasmin-2.4/jasmin.jar "$InFolder/$Name.j"
+java "$OutFolder/$Name"
+fi
--- tasks1234/kotlin/test/test_jasmin.j
+++ tasks1234/kotlin/test/test_jasmin.j
@@ -11,7 +11,7 @@
 .method public static main([Ljava/lang/String;)V
 	.limit stack 2
 	getstatic java/lang/System/out Ljava/io/PrintStream;
-	ldc "Hello world haha!"
+	ldc "Hello world wow!"
 	invokevirtual java/io/PrintStream/println(Ljava/lang/String;)V
 	return	
 .end method
