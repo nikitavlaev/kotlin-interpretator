04642b5587201f21287779541f0caaac14c735c8
Nikita <nikitavlaev00@gmail.com>
2020-01-26 02:24:34 +0300

    Fixed translator

--- tasks1234/kotlin/app/Translator.hs
+++ tasks1234/kotlin/app/Translator.hs
@@ -1,174 +1,177 @@
 {-# LANGUAGE DuplicateRecordFields #-}
 {-# LANGUAGE RecordWildCards #-}
 {-# LANGUAGE LambdaCase #-}
+{-# LANGUAGE BlockArguments #-}
 
 module Translator where
-import Ast
+import Prelude hiding (getChar)
+import Ast 
 import Control.Monad.Reader
 import Control.Monad.State.Lazy
-import System.IO
+import System.IO hiding (getChar)
 import Data.Tuple.Utils
 import Data.List
 
 type TableLocalVariables = [(String, KType, Int)] -- (name, type, index)
 
-pushStr :: String -> StateT TableLocalVariables (State [String]) KType
+pushStr :: String -> StateT TableLocalVariables (State [String]) ()
 pushStr newStr = lift $ modify (newStr : )
 
 translatorExpression :: Expr -> StateT TableLocalVariables (State [String]) KType
-translatorExpression = \case
-    Val (KDByte val) -> do
-        pushStr $ "bipush " ++ show val
-        return KTByte
-    Val (KDShort val) -> do
-        pushStr $ "sipush " ++ show val
-        return KTShort
-    Val (KDInt val) -> do
-        pushStr $ "ldc " ++ show val
-        return KTInt
-    Val (KDLong val) -> do
-        pushStr $ "ldc_w " ++ show val
-        return KTLong
-    Val (KDArray str@((KDChar c) : cs)) -> do
-        pushStr $ "ldc " ++ (getChar <$> str)
-        return $ KTArray KTChar
-    Val (KDDouble val) -> do
-        pushStr $ "ldc_w " ++ show val
-        return KTDouble
-    CallFun ".get" [Var varName] -> do
-        tableLocalVariables <- get
-        case find ((== varName) . fst3) tableLocalVariables of
-            Just var -> do
-                let ktype = snd3 var
-                pushStr $ case ktype of
-                    KTByte -> "iload " ++ show (trd3 var)
-                    KTShort -> "iload " ++ show (trd3 var)
-                    KTInt -> "iload " ++ show (trd3 var)
-                    KTLong -> "lload " ++ show (trd3 var)
-                    KTDouble -> "dload " ++ show (trd3 var)
-                    _ -> "aload " ++ show (trd3 var)
-                return ktype
-            Nothing -> undefined
-    Add e1 e2 -> do
-        ktype1 <- translatorExpression e1
-        ktype2 <- translatorExpression e2
-        case (ktype1, ktype2) of
-            (KTInt, KTInt) -> do
-                pushStr "iadd"
-                return KTInt
-            (KTLong, KTInt) -> do
-                pushStr "i2l"
-                pushStr "ladd"
-                return KTLong
-            (KTInt, KTLong) -> do
-                lengthTableLocalVariables <- gets length
-                pushStr $ "lstore " ++ show lengthTableLocalVariables
-                pushStr "i2l"
-                pushStr $ "lload " ++ show lengthTableLocalVariables
-                pushStr "ladd"
-                return KTLong
-            (KTLong, KTLong) -> do
-                pushStr "ladd"
-                return KTLong
-            (KTDouble, KTDouble) -> do
-                pushStr "dadd"
-                return KTLong
-    Sub e1 e2 -> do
-        ktype1 <- translatorExpression e1
-        ktype2 <- translatorExpression e2
-        case (ktype1, ktype2) of
-            (KTInt, KTInt) -> do
-                pushStr "isub"
-                return KTInt
-            (KTLong, KTInt) -> do
-                pushStr "i2l"
-                pushStr "lsub"
-                return KTLong
-            (KTInt, KTLong) -> do
-                lengthTableLocalVariables <- gets length
-                pushStr $ "lstore " ++ show lengthTableLocalVariables
-                pushStr "i2l"
-                pushStr $ "lload " ++ show lengthTableLocalVariables
-                pushStr "lsub"
-                return KTLong
-            (KTLong, KTLong) -> do
-                pushStr "lsub"
-                return KTLong
-            (KTDouble, KTDouble) -> do
-                pushStr "dsub"
-                return KTLong
-    Mul e1 e2 -> do
-        ktype1 <- translatorExpression e1
-        ktype2 <- translatorExpression e2
-        case (ktype1, ktype2) of
-            (KTInt, KTInt) -> do
-                pushStr "imul"
-                return KTInt
-            (KTLong, KTInt) -> do
-                pushStr "i2l"
-                pushStr "lmul"
-                return KTLong
-            (KTInt, KTLong) -> do
-                lengthTableLocalVariables <- gets length
-                pushStr $ "lstore " ++ show lengthTableLocalVariables
-                pushStr "i2l"
-                pushStr $ "lload " ++ show lengthTableLocalVariables
-                pushStr "lmul"
-                return KTLong
-            (KTLong, KTLong) -> do
-                pushStr "lmul"
-                return KTLong
-            (KTDouble, KTDouble) -> do
-                pushStr "dmul"
-                return KTLong
-    Div e1 e2 -> do
-        ktype1 <- translatorExpression e1
-        ktype2 <- translatorExpression e2
-        case (ktype1, ktype2) of
-            (KTInt, KTInt) -> do
-                pushStr "idiv"
-                return KTInt
-            (KTLong, KTInt) -> do
-                pushStr "i2l"
-                pushStr "ldiv"
-                return KTLong
-            (KTInt, KTLong) -> do
-                lengthTableLocalVariables <- gets length
-                pushStr $ "lstore " ++ show lengthTableLocalVariables
-                pushStr "i2l"
-                pushStr $ "lload " ++ show lengthTableLocalVariables
-                pushStr "ldiv"
-                return KTLong
-            (KTLong, KTLong) -> do
-                pushStr "ldiv"
-                return KTLong
-            (KTDouble, KTDouble) -> do
-                pushStr "ddiv"
-                return KTLong
-    Mod e1 e2 -> do
-        ktype1 <- translatorExpression e1
-        ktype2 <- translatorExpression e2
-        case (ktype1, ktype2) of
-            (KTInt, KTInt) -> do
-                pushStr "irem"
-                return KTInt
-            (KTLong, KTInt) -> do
-                pushStr "i2l"
-                pushStr "lrem"
-                return KTLong
-            (KTInt, KTLong) -> do
-                lengthTableLocalVariables <- gets length
-                pushStr $ "lstore " ++ show lengthTableLocalVariables
-                pushStr "i2l"
-                pushStr $ "lload " ++ show lengthTableLocalVariables
-                pushStr "lrem"
-                return KTLong
-            (KTLong, KTLong) -> do
-                pushStr "lrem"
-                return KTLong
-            (KTDouble, KTDouble) -> do
-                pushStr "drem"
-                return KTLong
+translatorExpression = 
+    \case 
+        {-Val (KDByte val) -> do
+            pushStr $ "bipush " ++ show val
+            return KTByte
+        Val (KDShort val) -> do
+            pushStr $ "sipush " ++ show val
+            return KTShort-}
+        Val (KDInt val) -> do
+            pushStr $ "ldc " ++ show val
+            return KTInt
+        {-Val (KDLong val) -> do
+            pushStr $ "ldc_w " ++ show val
+            return KTLong-}
+        Val (KDArray str@((KDChar c) : cs)) -> do
+            pushStr $ "ldc " ++ (getChar <$> str)
+            return $ KTArray KTChar
+        {-Val (KDDouble val) -> do
+            pushStr $ "ldc_w " ++ show val
+            return KTDouble-}
+        CallFun ".get" [Var varName] -> do
+            tableLocalVariables <- get
+            case find ((== varName) . fst3) tableLocalVariables of
+                Just var -> do
+                    let ktype = snd3 var
+                    pushStr $ case ktype of
+                        KTByte -> "iload " ++ show (thd3 var)
+                        KTShort -> "iload " ++ show (thd3 var)
+                        KTInt -> "iload " ++ show (thd3 var)
+                        KTLong -> "lload " ++ show (thd3 var)
+                        KTDouble -> "dload " ++ show (thd3 var)
+                        _ -> "aload " ++ show (thd3 var)
+                    return ktype
+                Nothing -> undefined
+        Add e1 e2 -> do
+            ktype1 <- translatorExpression e1
+            ktype2 <- translatorExpression e2
+            case (ktype1, ktype2) of
+                (KTInt, KTInt) -> do
+                    pushStr "iadd"
+                    return KTInt
+                (KTLong, KTInt) -> do
+                    pushStr "i2l"
+                    pushStr "ladd"
+                    return KTLong
+                (KTInt, KTLong) -> do
+                    lengthTableLocalVariables <- gets length
+                    pushStr $ "lstore " ++ show lengthTableLocalVariables
+                    pushStr "i2l"
+                    pushStr $ "lload " ++ show lengthTableLocalVariables
+                    pushStr "ladd"
+                    return KTLong
+                (KTLong, KTLong) -> do
+                    pushStr "ladd"
+                    return KTLong --if_icmpeq
+                (KTDouble, KTDouble) -> do
+                    pushStr "dadd"
+                    return KTLong
+        Sub e1 e2 -> do
+            ktype1 <- translatorExpression e1
+            ktype2 <- translatorExpression e2
+            case (ktype1, ktype2) of
+                (KTInt, KTInt) -> do
+                    pushStr "isub"
+                    return KTInt
+                (KTLong, KTInt) -> do
+                    pushStr "i2l"
+                    pushStr "lsub"
+                    return KTLong
+                (KTInt, KTLong) -> do
+                    lengthTableLocalVariables <- gets length
+                    pushStr $ "lstore " ++ show lengthTableLocalVariables
+                    pushStr "i2l"
+                    pushStr $ "lload " ++ show lengthTableLocalVariables
+                    pushStr "lsub"
+                    return KTLong
+                (KTLong, KTLong) -> do
+                    pushStr "lsub"
+                    return KTLong
+                (KTDouble, KTDouble) -> do
+                    pushStr "dsub"
+                    return KTLong
+        Mul e1 e2 -> do
+            ktype1 <- translatorExpression e1
+            ktype2 <- translatorExpression e2
+            case (ktype1, ktype2) of
+                (KTInt, KTInt) -> do
+                    pushStr "imul"
+                    return KTInt
+                (KTLong, KTInt) -> do
+                    pushStr "i2l"
+                    pushStr "lmul"
+                    return KTLong
+                (KTInt, KTLong) -> do
+                    lengthTableLocalVariables <- gets length
+                    pushStr $ "lstore " ++ show lengthTableLocalVariables
+                    pushStr "i2l"
+                    pushStr $ "lload " ++ show lengthTableLocalVariables
+                    pushStr "lmul"
+                    return KTLong
+                (KTLong, KTLong) -> do
+                    pushStr "lmul"
+                    return KTLong
+                (KTDouble, KTDouble) -> do
+                    pushStr "dmul"
+                    return KTLong
+        Div e1 e2 -> do
+            ktype1 <- translatorExpression e1
+            ktype2 <- translatorExpression e2
+            case (ktype1, ktype2) of
+                (KTInt, KTInt) -> do
+                    pushStr "idiv"
+                    return KTInt
+                (KTLong, KTInt) -> do
+                    pushStr "i2l"
+                    pushStr "ldiv"
+                    return KTLong
+                (KTInt, KTLong) -> do
+                    lengthTableLocalVariables <- gets length
+                    pushStr $ "lstore " ++ show lengthTableLocalVariables
+                    pushStr "i2l"
+                    pushStr $ "lload " ++ show lengthTableLocalVariables
+                    pushStr "ldiv"
+                    return KTLong
+                (KTLong, KTLong) -> do
+                    pushStr "ldiv"
+                    return KTLong
+                (KTDouble, KTDouble) -> do
+                    pushStr "ddiv"
+                    return KTLong
+        Mod e1 e2 -> do
+            ktype1 <- translatorExpression e1
+            ktype2 <- translatorExpression e2
+            case (ktype1, ktype2) of
+                (KTInt, KTInt) -> do
+                    pushStr "irem"
+                    return KTInt
+                (KTLong, KTInt) -> do
+                    pushStr "i2l"
+                    pushStr "lrem"
+                    return KTLong
+                (KTInt, KTLong) -> do
+                    lengthTableLocalVariables <- gets length
+                    pushStr $ "lstore " ++ show lengthTableLocalVariables
+                    pushStr "i2l"
+                    pushStr $ "lload " ++ show lengthTableLocalVariables
+                    pushStr "lrem"
+                    return KTLong
+                (KTLong, KTLong) -> do
+                    pushStr "lrem"
+                    return KTLong
+                (KTDouble, KTDouble) -> do
+                    pushStr "drem"
+                    return KTLong
 
 {-
 type ConstantPool = [Constant]
@@ -222,9 +225,10 @@ translatorClass (Class {..}) = do
             attributes = undefined
         }
 
+-} 
 translateHelloWorld :: String -> IO()
 translateHelloWorld name = do 
     helloWorldj <- readFile "test/hello_world.j"
     writeFile (name ++ ".j") helloWorldj
     return ()
--}
+
