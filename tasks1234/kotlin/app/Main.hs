e4b5e8e9e58137061db1ed5e4ed3c4af9c0ae6c5
Nikita <nikitavlaev00@gmail.com>
2020-01-26 13:26:50 +0300

    Added way to test translator

--- tasks1234/kotlin/app/Main.hs
+++ tasks1234/kotlin/app/Main.hs
@@ -8,6 +8,8 @@ import Translator
 import Interpreter
 import Text.Parsec(parse)
 import Text.Pretty.Simple (pPrint)
+import Control.Monad.State.Lazy
+
 
 runProgram :: (Show a) => Either a Class -> IO()
 runProgram (Right cl) = interpretProgram cl
@@ -18,8 +20,17 @@ main = do
     args <- getArgs
     case args of
         ("t":name:other) -> do
-            translateHelloWorld name
-            putStrLn $ "Translator finished " ++ name
+            {-translateHelloWorld name
+            putStrLn $ "Translator finished " ++ name-}
+            program <- readFile "test/test_translator.kt"
+            putStrLn $ removeComments program 0
+            ast <- return $ parse parseProgram "" $ removeComments program 0
+            pPrint ast 
+            case ast of 
+                (Right (Class _ _ ((Fun _ _ _ (((Expression expr):next)):other)) _ )) -> do 
+                                                let result = execState $ (runStateT $ translatorExpression expr) [] 
+                                                pPrint $ result []
+                _ -> putStrLn "Unsupported program"                                 
         ("i":other) -> do       
             program <- readFile "test/test_program.kt"
             putStrLn $ removeComments program 0
--- tasks1234/kotlin/test/test_translator.kt
+++ tasks1234/kotlin/test/test_translator.kt
@@ -1,3 +1,3 @@
 fun Main() {
-  println("Hello!")
+  1 + 2
 }
